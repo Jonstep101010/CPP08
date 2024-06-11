2954b29a8a06e48128011cf84cbb79c61788305d
jschwabe <mail@jschwabe.site>
2024-06-11 17:44:34 +0200

    ex02: add additional containers as examples

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,8 +1,59 @@
 #include "MutantStack.hpp"
-#include <algorithm>
 #include <iostream>
 #include <list>
 #include <vector>
+
+// clang-format off
+void testlist() {
+
+	std::cout << "other container: list" << std::endl;
+	MutantStack<int, std::list<int> > mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int, std::list<int> >::iterator it  = mstack.begin();
+	MutantStack<int, std::list<int> >::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int, std::list<int> > s(mstack);
+}
+
+void testvec() {
+
+	std::cout << "other container: vector" << std::endl;
+	MutantStack<int, std::vector<int> > mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int, std::vector<int> >::iterator it  = mstack.begin();
+	MutantStack<int, std::vector<int> >::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int, std::vector<int> > s(mstack);
+}
+// clang-format on
 
 int main() {
 	MutantStack<int> mstack;
@@ -25,5 +76,7 @@ int main() {
 		++it;
 	}
 	std::stack<int> s(mstack);
+	testlist();
+	testvec();
 	return 0;
 }

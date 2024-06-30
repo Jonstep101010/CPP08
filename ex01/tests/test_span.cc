eaa68696e7b374386aa3b37af3201a88e4958efd
jschwabe <mail@jschwabe.site>
2024-06-30 13:11:44 +0200

    chore: create example that reflects the main used for testing with googletest
    Signed-off-by: jschwabe <mail@jschwabe.site>

---
+++ ex01/tests/test_span.cc
@@ -0,0 +1,21 @@
+// the test has to be written in cpp14, but the library is written in cpp98
+
+#include <gtest/gtest.h>
+#include <random>
+
+#include "Span.hpp"
+
+TEST(SpanTest, test01) {
+	std::vector<int> v;
+
+	for (int i = 0; i <= 10000; i++) {
+		v.push_back(i);
+	}
+	std::shuffle(v.begin(), v.end(), std::default_random_engine());
+	Span sp = Span(10001);
+
+	sp.addRange(v.begin(), v.end());
+
+	EXPECT_EQ(sp.shortestSpan(), 1);
+	EXPECT_EQ(sp.longestSpan(), 10000);
+}
